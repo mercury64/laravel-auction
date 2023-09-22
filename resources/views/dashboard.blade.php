af6a84d3263aced250c819e1730e3d345dbb3574
Warren White <wew@sidneybc.ca>
2023-09-21 20:46:23 -0700

    Removed Your Balance, gaurded add money

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -10,8 +10,6 @@
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
                     @include('layouts.success-message')
-                    <h3 class="mb-4 font-semibold text-lg text-gray-800 leading-tight">You balance:</h3>
-                    <div class="text-green-500 text-5xl my-2">${{ Auth::user()->balance }}</div>
                     <h3 class="mb-4 font-semibold text-lg text-gray-800 leading-tight">You purchases:</h3>
                     <user-purchases></user-purchases>
                     <a
@@ -20,13 +18,14 @@
                     bg-yellow-500 rounded-lg focus:shadow-outline hover:bg-yellow-600">
                         Edit profile
                     </a>
+                    @can('edit users')
                     <a
                         href="{{ route('profile.addMoney') }}"
                         class="flex justify-center items-center h-10 w-40 px-5 mt-3 mr-5 text-gray-100 transition-colors duration-200
                     bg-green-500 rounded-lg focus:shadow-outline hover:bg-green-600">
                         Top up balance
                     </a>
-                    @can('edit users')
+                   
                         <div class="flex">
                             <a
                                 href="{{ route('admin.users.index') }}"
