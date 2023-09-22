1b7520b9e3b3307642a13473b1121b845cbee085
Warren White <wew@sidneybc.ca>
2023-09-21 20:46:57 -0700

    Changed home page name

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Home page') }}
+            {{ __('SNSYC Auction') }}
         </h2>
     </x-slot>
 
